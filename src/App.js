bd5b139c0b559a091f6a91a5cf0b735477343dd7
Opone Chukwuyenum <oponechukwuyenum@icloud.com>
2021-08-25 10:05:06 +0100

    Expense Form created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from 'react';
+
+import Expenses from "../src/components/Expenses/Expenses";
+import NewExpense from "../src/components/NewExpense/NewExpense";
+
 function App() {
+  const expenses = [
+    { date: new Date(2021, 8, 22), title: "Car Insurance", amount: 228.77 },
+    { date: new Date(2020, 8, 22), title: "Car Policy", amount: 208.89 },
+    { date: new Date(2021, 2, 22), title: "Car Agency", amount: 227.95 },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
+      <NewExpense/>
+      <Expenses expenses={expenses}></Expenses>
     </div>
   );
 }
